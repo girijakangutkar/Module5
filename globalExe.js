324503bbb984557de90f719ffbfaaa60592552a2
Girijakangutkar <girijakangutkar2002@gmail.com>
2025-04-24 00:26:28 +0530

    module5 submission

---
+++ globalExe.js
@@ -0,0 +1,15 @@
+let age = 23;
+
+function displayAge() {
+  console.log(age);
+}
+
+displayAge();
+
+function changeAge() {
+  let newAge = 24;
+  age = newAge;
+  console.log(age);
+}
+
+changeAge();
