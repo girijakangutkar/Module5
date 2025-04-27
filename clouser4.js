d0b603a6ceedf74838739446c1f73aefc0fb6484
Girijakangutkar <girijakangutkar2002@gmail.com>
2025-04-27 10:24:27 +0530

    adding clouser 4

--- clouser4.js
+++ clouser4.js
@@ -2,13 +2,24 @@ function createFunctionList() {
   let functions = [];
 
   for (var i = 0; i < 5; i++) {
-    functions.push(
-        indexing: function () {
-      console.log("Index:", i);
+    functions.push({
+      indexing: function () {
+        console.log("Index:", i);
+      },
     });
   }
 
   return functions;
 }
 
-console.log(createFunctionList());
+const functionList = createFunctionList();
+
+functionList[0].indexing();
+
+functionList[1].indexing();
+
+functionList[2].indexing();
+
+functionList[3].indexing();
+
+functionList[4].indexing();
